17ccd9dd9a9deaac60830de0e095533eb8425c1f
Dev Bash <ikorosamuel1@gmail.com>
2023-11-25 22:04:45 +0100

    Update .env and CSS files, and refactor DashboardLayout and Chat components

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -3,21 +3,23 @@ import { DashboardHeader, DashboardSidebar } from "../molecules";
 import { NavProvider } from "@/context/nav_context";
 
 export const DashboardLayout = ({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) => {
-  return (
-    <NavProvider>
-      <main className="bg-white min-h-full flex !oveflow-hidden">
-        <DashboardSidebar />
+    return (
+        <NavProvider>
+            <main className="bg-white min-h-full flex !oveflow-hidden">
+                <DashboardSidebar />
 
-        <section className="w-full h-full grid gap-6 relative overf">
-          <DashboardHeader />
+                <section className="w-full h-full grid gap-6 relative overf">
+                    <DashboardHeader />
 
-          <section className="py-6 px-[MIN(100px,5%)]">{children}</section>
-        </section>
-      </main>
-    </NavProvider>
-  );
+                    <section className="py-6 px-[MIN(100px,5%)]">
+                        {children}
+                    </section>
+                </section>
+            </main>
+        </NavProvider>
+    );
 };
