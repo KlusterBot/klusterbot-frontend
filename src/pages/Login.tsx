0ca0538dbef1fb2367f005e8a1c9895beb4a2fe0
Dev Bash <ikorosamuel1@gmail.com>
2023-11-27 00:07:54 +0100

    Refactor Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,77 +11,86 @@ import { login } from "../lib/services";
 import { getError } from "../lib/utils";
 
 const initialValues = {
-  email: "",
-  password: "",
+    email: "",
+    password: "",
 };
 
 export const Login = () => {
-  // const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+    // const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-  const { handleBlur, handleSubmit, handleChange, values, touched, errors } =
-    useFormik({
-      initialValues,
-      validationSchema: signInValidationSchema,
-      onSubmit: async () => {
-        setLoading(true);
-        try {
-          const { data, error } = await login(values);
-          if (error) {
-            throw new Error(error);
-          }
-          if (data) {
-            toast.success("Login successful.");
-            setLoading(false);
-            !data.data?.verified ? navigate("/setup") : navigate("/dashboard");
-          }
-        } catch (error) {
-          setLoading(false);
-          toast.error(getError(error));
-          // setError(getError(error));
-        }
-      },
-    });
+    const { handleBlur, handleSubmit, handleChange, values, touched, errors } =
+        useFormik({
+            initialValues,
+            validationSchema: signInValidationSchema,
+            onSubmit: async () => {
+                setLoading(true);
+                try {
+                    const { data, error } = await login(values);
+                    if (error) {
+                        throw new Error(error);
+                    }
+                    if (data) {
+                        toast.success("Login successful.");
+                        setLoading(false);
+                        localStorage.setItem("user", JSON.stringify(data.data));
+                        !data.data?.verified
+                            ? navigate("/setup")
+                            : navigate("/dashboard");
+                    }
+                } catch (error) {
+                    setLoading(false);
+                    toast.error(getError(error));
+                    // setError(getError(error));
+                }
+            },
+        });
 
-  return (
-    <AuthForm onSubmit={handleSubmit}>
-      <Input
-        name="email"
-        id="email"
-        label="Enter Email Address"
-        placeholder="Enter Email Address"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.email}
-        error={touched.email && errors.email ? errors.email : ""}
-      />
-      <Input
-        name="password"
-        id="password"
-        label="Enter password"
-        placeholder="Enter password"
-        type="password"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.password}
-        error={touched.password && errors.password ? errors.password : ""}
-      />
+    return (
+        <AuthForm onSubmit={handleSubmit}>
+            <Input
+                name="email"
+                id="email"
+                label="Enter Email Address"
+                placeholder="Enter Email Address"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                error={touched.email && errors.email ? errors.email : ""}
+            />
+            <Input
+                name="password"
+                id="password"
+                label="Enter password"
+                placeholder="Enter password"
+                type="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                error={
+                    touched.password && errors.password ? errors.password : ""
+                }
+            />
 
-      {/* {error && <p className="my-1 text-center text-red-500">{error}</p>} */}
+            {/* {error && <p className="my-1 text-center text-red-500">{error}</p>} */}
 
-      <Button loading={loading} type="submit" className="mx-auto w-full mt-6">
-        Log in
-      </Button>
+            <Button
+                loading={loading}
+                type="submit"
+                className="mx-auto w-full mt-6"
+            >
+                Log in
+            </Button>
 
-      <div className="flex flex-wrap justify-center items-center w-full gap-1">
-        <p className="text-center">Don't have account? Create one</p>
-        <Link to="/signup" className="text-dark-blue-color font-bold">
-          Here
-        </Link>
-      </div>
+            <div className="flex flex-wrap justify-center items-center w-full gap-1">
+                <p className="text-center">Don't have account? Create one</p>
+                <Link to="/signup" className="text-dark-blue-color font-bold">
+                    Here
+                </Link>
+            </div>
 
-      {/* <div className="w-full grid gap-4 py-4 p-2 ">
+            {/* <div className="w-full grid gap-4 py-4 p-2 ">
         <p className="w-full flex items-center">
           <span className="border-t w-full" />
           <span className="mx-2">or</span>
@@ -95,6 +104,6 @@ export const Login = () => {
           <p className="mx-auto">Sign in with Google</p>
         </Button>
       </div> */}
-    </AuthForm>
-  );
+        </AuthForm>
+    );
 };
